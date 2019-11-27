20a5a38d0654008591c5f3491ccae9db6047fd2b
Rio Yokota <rioyokota@gmail.com>
2019-11-27 22:13:19 +0900

    Making test_serial_ij.f90 work again.

--- include/kernel.h
+++ include/kernel.h
@@ -193,6 +193,7 @@ public:
     }                                                           // End loop over each dimension
     R0 += 1e-5;                                                 // Add some leeway to root radius
     if( IMAGES != 0 ) {                                         // If periodic boundary condition
+      R0 -= 1e-5;                                               //  Prevent superfluous error
       if( X0[0]-R0 < x0[0]-r0 || x0[0]+r0 < X0[0]+R0            //  Check for outliers in x direction
        || X0[1]-R0 < x0[1]-r0 || x0[1]+r0 < X0[1]+R0            //  Check for outliers in y direction
        || X0[2]-R0 < x0[2]-r0 || x0[2]+r0 < X0[2]+R0 ) {        //  Check for outliers in z direction
--- wrapper/test_serial_ij.f90
+++ wrapper/test_serial_ij.f90
@@ -11,51 +11,52 @@ program main
   kappa = 1.003078798D-7
   kappa4pi=7.982247452D-9
 
-  open(50,file='fort.50')
-  read(50,*) ni
+!  open(50,file='fort.50')
+!  read(50,*) ni
 
-  open(51,file='fort.51')
-  read(51,*) nj, npl
-  nj = nj*npl
+!  open(51,file='fort.51')
+!  read(51,*) nj, npl
+!  nj = nj*npl
 
-!  ni = 1000
-!  nj = 20000
-!  nj = 80000
-!  nj = 640*8
-!  nj = 8
-!  nj = 8*2
-!  nj = 8*3
+  ni = 10000
+  nj = 20000
   allocate( xi(3*ni),ui(3*ni),ud(3*ni),xj(3*nj),gj(3*nj) )
-!  do i = 1,128
-!     iseed(i) = 0
-!  enddo
-!  call random_seed(put=iseed)
-!  call random_number(xi)
-!  call random_number(xj)
-!  call random_number(gj)
+  do i = 1,128
+     iseed(i) = 0
+  enddo
+  call random_seed(put=iseed)
+  call random_number(xi)
+  call random_number(xj)
+  call random_number(gj)
 
   do i = 1,nj
-!     gj(3*i-2) = (gj(3*i-2) - 0.5) / ni
-!     gj(3*i-1) = (gj(3*i-1) - 0.5) / ni
-!     gj(3*i-0) = (gj(3*i-0) - 0.5) / ni
-     read(51,*) xj(3*i-2), gj(3*i-2)
-     read(51,*) xj(3*i-1), gj(3*i-1)
-     read(51,*) xj(3*i-0), gj(3*i-0)
+     xj(3*i-2) = (xj(3*i-2) - 0.5) * pi
+     xj(3*i-1) = (xj(3*i-1) - 0.5) * pi
+     xj(3*i-0) = (xj(3*i-0) - 0.5) * pi
+     gj(3*i-2) = (gj(3*i-2) - 0.5) / ni
+     gj(3*i-1) = (gj(3*i-1) - 0.5) / ni
+     gj(3*i-0) = (gj(3*i-0) - 0.5) / ni
+!     read(51,*) xj(3*i-2), gj(3*i-2)
+!     read(51,*) xj(3*i-1), gj(3*i-1)
+!     read(51,*) xj(3*i-0), gj(3*i-0)
   enddo
-  close(51)
+!  close(51)
 
   do i = 1,ni
+     xi(3*i-2) = (xi(3*i-2) - 0.5) * pi
+     xi(3*i-1) = (xi(3*i-1) - 0.5) * pi
+     xi(3*i-0) = (xi(3*i-0) - 0.5) * pi
      ui(3*i-2) = 0
      ui(3*i-1) = 0
      ui(3*i-0) = 0
      ud(3*i-2) = 0
      ud(3*i-1) = 0
      ud(3*i-0) = 0
-     read(50,*) xi(3*i-2)
-     read(50,*) xi(3*i-1)
-     read(50,*) xi(3*i-0)
+!     read(50,*) xi(3*i-2)
+!     read(50,*) xi(3*i-1)
+!     read(50,*) xi(3*i-0)
   enddo
-  close(50)
+!  close(50)
 
   images = 3
   call fmm_init(images)
@@ -72,12 +73,6 @@ program main
      norm = norm + ud(3*i-0) ** 2
   enddo
   print '(a,es12.5)',"error         :",sqrt(diff/norm)
-!  print '(a,3es15.6)',"ui(1:3) ",ui(1),ui(2),ui(3)
-!  print '(a,3es15.6)',"ud(1:3) ",ud(1),ud(2),ud(3)
-!  print '(a,3es15.6)',"ui(1:3) ",ui(1)*4*pi,ui(2)*4*pi,ui(3)*4*pi
-!  print '(a,3es15.6)',"ud(1:3) ",ud(1)*4*pi,ud(2)*4*pi,ud(3)*4*pi
-  print '(a,3es15.6)',"ui(1:3) ",ui(1)*kappa,ui(2)*kappa,ui(3)*kappa
-  print '(a,3es15.6)',"ud(1:3) ",ud(1)*kappa,ud(2)*kappa,ud(3)*kappa
   call fmm_finalize()
   deallocate( xi,ui,ud,xj,gj )
 end program main
