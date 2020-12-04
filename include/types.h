64eb0daa10962c81f8ceb14baadebe8a8f0a7fdd
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2020-12-04 21:33:12 +0900

    new test config

--- include/types.h
+++ include/types.h
@@ -33,16 +33,16 @@ extern int MPISIZE;                                             //!< MPI comm si
 #endif
 
 const int  P       = 10;                                        //!< Order of expansions
-const int  NCRIT   = 30000;                                      //!< Number of bodies per cell
+//const int  NCRIT   = 30000;                                      //!< Number of bodies per cell
 //const int  NCRIT   = 6000;                                      //!< Number of bodies per cell
-//const int  NCRIT   = 1000;                                      //!< Number of bodies per cell
+const int  NCRIT   = 1000;                                      //!< Number of bodies per cell
 //const int  NCRIT   = 500;                                      //!< Number of bodies per cell
 //const int  NCRIT   = 50;                                      //!< Number of bodies per cell
 const int  MAXBODY = 200000;                                    //!< Maximum number of bodies per GPU kernel
 const int  MAXCELL = 10000000;                                  //!< Maximum number of bodies/coefs in cell per GPU kernel
 const real CLET    = 2;                                         //!< LET opening critetia
-//const real EPS2    = 1e-6;                                      //!< Softening parameter
-const real EPS2    = 1e-30;                                      //!< Softening parameter
+const real EPS2    = 1e-6;                                      //!< Softening parameter
+//const real EPS2    = 1e-30;                                      //!< Softening parameter
 const int  GPUS    = 4;                                         //!< Number of GPUs per node
 const int  THREADS = 64;                                        //!< Number of threads per thread-block
 
