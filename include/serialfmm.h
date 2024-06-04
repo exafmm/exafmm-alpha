d9dbbced3fe8879e9b91f957a436a5b23191961e
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2024-06-04 14:08:50 +0900

    Remove topdown tree construction.

--- include/serialfmm.h
+++ include/serialfmm.h
@@ -1,13 +1,12 @@
 #ifndef serialfmm_h
 #define serialfmm_h
-#include "topdown.h"
 #include "bottomup.h"
 
 //! Serial FMM interface
-class SerialFMM : public TopDown, public BottomUp {
+class SerialFMM : public BottomUp {
 public:
 //! Constructor
-  SerialFMM() : TopDown(), BottomUp() {
+  SerialFMM() : BottomUp() {
     preCalculation();
   }
 //! Destructor
@@ -15,27 +14,11 @@ public:
     postCalculation();
   }
 
-//! Topdown tree constructor interface. Input: bodies, Output: cells
-  void topdown(Bodies &bodies, Cells &cells) {
-    TopDown::grow(bodies);                                      // Grow tree structure topdown
-
-    TopDown::setIndex();                                        // Set index of cells
-
-    buffer.resize(bodies.size());                             // Resize sort buffer
-    sortBodies(bodies,buffer,false);                            // Sort bodies in descending order
-
-    Cells twigs;                                                // Twigs are cells at the bottom of tree
-    bodies2twigs(bodies,twigs);                                 // Turn bodies to twigs
-
-    Cells sticks;                                               // Sticks are twigs from other processes that are not twigs in the current process
-    twigs2cells(twigs,cells,sticks);                            // Turn twigs to cells
-  }
-
 //! Bottomup tree constructor interface. Input: bodies, Output: cells
   void bottomup(Bodies &bodies, Cells &cells) {
     BottomUp::setIndex(bodies);                                 // Set index of cells
 
-    buffer.resize(bodies.size());                             // Resize sort buffer
+    buffer.resize(2*bodies.size());                             // Resize sort buffer
     sortBodies(bodies,buffer,false);                            // Sort bodies in descending order
 
 /*
